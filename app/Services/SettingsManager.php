3ad5592d4b988bd5e89ee1d4c7fa9fa570c0bb69
lampdev <orders@lamp-dev.com>
2023-04-28 15:43:49 +0300

    Settings with Tenant

---
+++ app/Services/SettingsManager.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Services;
+
+use anlutro\LaravelSettings\Facades\Setting;
+use anlutro\LaravelSettings\SettingsManager as BaseSettingsManager;
+
+class SettingsManager extends BaseSettingsManager
+{
+    public function allByTenant($tenantId)
+    {
+        Setting::setExtraColumns(['tenant_id' => $tenantId]);
+        return Setting::all();
+    }
+
+    public function getByTenant($key, $tenantId, $default = null)
+    {
+        Setting::setExctraColumns(['tenant_id' => $tenantId]);
+
+        return Setting::get($key, $default);
+    }
+
+    public function setByTenant($key, $value, $tenantId): void
+    {
+        Setting::setExtraColumns(['tenant_id' => $tenantId]);
+        Setting::set($key, $value);
+    }
+}
