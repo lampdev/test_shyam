62145b09cfbe797306c7d936c1a18094a8938dd5
lampdev <orders@lamp-dev.com>
2023-04-28 11:09:36 +0300

    User factory&seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,10 +3,13 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 class UserFactory extends Factory
 {
+    const DEFAULT_PASSWORD = 'password';
+
     /**
      * Define the model's default state.
      *
@@ -18,7 +21,7 @@ class UserFactory extends Factory
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => Hash::make(self::DEFAULT_PASSWORD),
             'remember_token' => Str::random(10),
         ];
     }
